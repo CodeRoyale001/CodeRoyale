b764ad0580fe47e5009647359a005206e3e54b47
Kartikay Sharma <sharmakartikay786@gmail.com>
2024-01-01 22:37:02 +0530

    navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,31 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Input } from "@/components/ui/input";
 
 const Navbar = () => {
   return (
-    <Button>Login</Button>
-  )
-}
+    <>
+      <div className=" navbar-container flex flex-row gap-10 justify-between p-5  bg-foreground text-background">
+        <div className="logo w-1/4">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </div>
+        <div className="flex justify-evenly gap-7">
+          <div className="search-nav">
+            <Input />
+          </div>
+          <ul className="flex gap-5 ">
+            <li>Contest</li>
+            <li>Leaderboard</li>
+            <li>Practice</li>
+          </ul>
+          <Button>Login</Button>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (<>
-  <h1>Hello World</h1>
+  
   <Navbar />
   </>
   )
