b2fef55608ac23c3cce048217bc4beaacb371215
jals413 <jalsthakor413@gmail.com>
2024-01-01 22:55:20 +0530

    navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,31 +1,61 @@
 import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Link from "next/link";
 import { Input } from "@/components/ui/input";
+import {
+	NavigationMenu,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuList,
+	navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
 
 const Navbar = () => {
-  return (
-    <>
-      <div className=" navbar-container flex flex-row gap-10 justify-between p-5  bg-foreground text-background">
-        <div className="logo w-1/4">
-          <Avatar>
-            <AvatarImage src="https://github.com/shadcn.png" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-        </div>
-        <div className="flex justify-evenly gap-7">
-          <div className="search-nav">
-            <Input />
-          </div>
-          <ul className="flex gap-5 ">
-            <li>Contest</li>
-            <li>Leaderboard</li>
-            <li>Practice</li>
-          </ul>
-          <Button>Login</Button>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className=" navbar-container flex flex-row gap-10 justify-between p-5  bg-background text-foreground">
+				<Avatar>
+					<AvatarImage src="https://github.com/shadcn.png" />
+					<AvatarFallback>CN</AvatarFallback>
+				</Avatar>
+				<Input />
+				<NavigationMenu className="gap-5">
+					<NavigationMenuList>
+						<NavigationMenuItem>
+							<Link href="/docs" legacyBehavior passHref>
+								<NavigationMenuLink
+									className={navigationMenuTriggerStyle()}
+								>
+									Contest
+								</NavigationMenuLink>
+							</Link>
+						</NavigationMenuItem>
+
+						<NavigationMenuItem>
+							<Link href="/docs" legacyBehavior passHref>
+								<NavigationMenuLink
+									className={navigationMenuTriggerStyle()}
+								>
+									Practice
+								</NavigationMenuLink>
+							</Link>
+						</NavigationMenuItem>
+
+						<NavigationMenuItem>
+							<Link href="/docs" legacyBehavior passHref>
+								<NavigationMenuLink
+									className={navigationMenuTriggerStyle()}
+								>
+									Leaderboard
+								</NavigationMenuLink>
+							</Link>
+						</NavigationMenuItem>
+					</NavigationMenuList>
+				</NavigationMenu>
+				<Button>Login</Button>
+			</div>
+		</>
+	);
 };
 
 export default Navbar;
