31d26ba3677cd46b2785c0a645d4f13aad139d2c
jals413 <jalsthakor413@gmail.com>
2024-01-02 01:35:12 +0530

    test

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -10,19 +10,57 @@ import {
 	navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 
+import {
+	Command,
+	CommandDialog,
+	CommandEmpty,
+	CommandGroup,
+	CommandInput,
+	CommandItem,
+	CommandList,
+	CommandSeparator,
+	CommandShortcut,
+  } from "@/components/ui/command"
+
 const Navbar = () => {
+	const handleLogin = () => {
+		console.log("login");
+	};
+
 	return (
 		<>
-			<div className=" navbar-container flex flex-row gap-10 justify-between p-5  bg-background text-foreground">
-				<Avatar>
-					<AvatarImage src="https://github.com/shadcn.png" />
-					<AvatarFallback>CN</AvatarFallback>
-				</Avatar>
-				<Input />
+			<div className=" navbar-container flex flex-row gap-10 justify-between p-5 bg-background text-foreground border-b">
+				<Link href="/" legacyBehavior passHref>
+					<div>
+						<Avatar>
+							<AvatarImage src="https://github.com/shadcn.png" />
+							<AvatarFallback>CN</AvatarFallback>
+						</Avatar>
+					</div>
+				</Link>
+
+				<Command>
+					<CommandInput placeholder="Type a command or search..." />
+					<CommandList>
+						<CommandEmpty>No results found.</CommandEmpty>
+						<CommandGroup heading="Suggestions">
+							<CommandItem>Calendar</CommandItem>
+							<CommandItem>Search Emoji</CommandItem>
+							<CommandItem>Calculator</CommandItem>
+						</CommandGroup>
+						<CommandSeparator />
+						<CommandGroup heading="Settings">
+							<CommandItem>Profile</CommandItem>
+							<CommandItem>Billing</CommandItem>
+							<CommandItem>Settings</CommandItem>
+						</CommandGroup>
+					</CommandList>
+				</Command>
+
 				<NavigationMenu className="gap-5">
 					<NavigationMenuList>
 						<NavigationMenuItem>
-							<Link href="/docs" legacyBehavior passHref>
+							<Link href="/contests" legacyBehavior passHref>
 								<NavigationMenuLink
 									className={navigationMenuTriggerStyle()}
 								>
@@ -32,7 +70,7 @@ const Navbar = () => {
 						</NavigationMenuItem>
 
 						<NavigationMenuItem>
-							<Link href="/docs" legacyBehavior passHref>
+							<Link href="/problems" legacyBehavior passHref>
 								<NavigationMenuLink
 									className={navigationMenuTriggerStyle()}
 								>
@@ -42,7 +80,7 @@ const Navbar = () => {
 						</NavigationMenuItem>
 
 						<NavigationMenuItem>
-							<Link href="/docs" legacyBehavior passHref>
+							<Link href="/leaderboard" legacyBehavior passHref>
 								<NavigationMenuLink
 									className={navigationMenuTriggerStyle()}
 								>
@@ -52,7 +90,7 @@ const Navbar = () => {
 						</NavigationMenuItem>
 					</NavigationMenuList>
 				</NavigationMenu>
-				<Button>Login</Button>
+				<Button onlclick={handleLogin}>Login / SignUp</Button>
 			</div>
 		</>
 	);
---
+++ src/pages/problems.js
@@ -0,0 +1,10 @@
+import Navbar from "@/components/navbar";
+
+export default function login() {
+	return (
+		<>
+        <Navbar/>
+			<h1>This is problems page</h1>
+		</>
+	);
+}
