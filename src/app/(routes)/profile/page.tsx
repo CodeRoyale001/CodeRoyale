994f2ca2b85b5ac0cef31496721708105431ce4f
Ajeet Kumar <ajeet8364@gmail.com>
2024-05-27 23:14:27 +0530

    -added UserDetails on Profile Page

--- src/app/(routes)/profile/page.tsx
+++ src/app/(routes)/profile/page.tsx
@@ -1,18 +1,20 @@
 "use client"
-import Navbar from "@/components/navbar";
+import { getRequest } from "@/utils/api";
+import { getCookie } from "@/utils/cookies";
 import { useState, useEffect } from "react";
+type UserDetails = {
+  userName: string;
+  firstName: string;
+  lastName: string;
+  userEmail: string;
+  userPhone: string;
+  userCountry: string;
+  userRole: number;
+  userInstitute: string;
+};
 
 export default function Profile() {
-  const [userDetails, setUserDetails] = useState({
-    userName: "",
-    firstName: "",
-    lastName: "",
-    email: "",
-    phone: "",
-    country: "",
-    role: "",
-    institute: "",
-  });
+  const [userDetails, setUserDetails] = useState<UserDetails>({} as UserDetails);
 
   useEffect(() => {
     fetchUserDetails()
@@ -20,18 +22,34 @@ export default function Profile() {
       .catch(error => console.error("Error fetching user details:", error));
   }, []);
 
-  async function fetchUserDetails() {
+  async function fetchUserDetails(): Promise<UserDetails> {
     try {
-      const response = await fetch("http://localhost:5005/getUser");
-      if (!response.ok) {
-        throw new Error("Failed to fetch user details");
+      const url = process.env.JS_URI + "/user/getUser";
+      const accessToken = getCookie("accessToken");
+  
+      if (!accessToken) {
+        throw new Error('Please Login');
       }
-      const data = await response.json();
+  
+      const dataPromise: Promise<UserDetails> = new Promise((resolve, reject) => {
+        getRequest(url, accessToken, (data: UserDetails) => {
+          if (data) {
+            resolve(data);
+          } else {
+            reject(new Error("Failed to fetch user details"));
+          }
+        });
+      });
+  
+      const data = await dataPromise;
       return data;
     } catch (error) {
+      console.error('Error fetching user details:', error);
       throw new Error("Error fetching user details");
     }
   }
+  
+  
 
   return (
     <>
@@ -40,11 +58,11 @@ export default function Profile() {
         <p>User Name: {userDetails.userName}</p>
         <p>First Name: {userDetails.firstName}</p>
         <p>Last Name: {userDetails.lastName}</p>
-        <p>Email: {userDetails.email}</p>
-        <p>Phone: {userDetails.phone}</p>
-        <p>Country: {userDetails.country}</p>
-        <p>Role: {userDetails.role}</p>
-        <p>Institute: {userDetails.institute}</p>
+        <p>Email: {userDetails.userEmail}</p>
+        <p>Phone: {userDetails.userPhone}</p>
+        <p>Country: {userDetails.userPhone}</p>
+        <p>Role: {userDetails.userCountry}</p>
+        <p>Institute: {userDetails.userCountry}</p>
       </div>
     </>
   );
