970b1e68ede71e55efe472ddf2c43a357113a8a1
Armaan Mishra <armaanmishra48@gmail.com>
2024-07-18 02:18:55 +0530

    added media queries

--- src/components/popups/LoginPopup.tsx
+++ src/components/popups/LoginPopup.tsx
@@ -8,53 +8,58 @@ import { SignUpForm } from "@/components/forms/signupForm";
 import { LoginForm } from "@/components/forms/loginForm";
 
 interface LoginPopupProps {
-	btntext: string;
-	btnVaraint: any;
-	icon?: boolean;
-	classname?: string;
+  btntext: string;
+  btnVaraint: any;
+  icon?: boolean;
+  classname?: string;
 }
 
-const LoginPopup: React.FC<LoginPopupProps> = ({ btntext, btnVaraint, icon = true, classname = '' }) => {
-	return (
-		<>
-			<Dialog>
-				<DialogTrigger asChild>
-					<Button variant={btnVaraint} className={classname}>
-						{ icon && <Login size="32" className="pr-2" /> }
-						{btntext}
-					</Button>
-				</DialogTrigger>
-				<DialogContent className="justify-center pt-10 w-[475px]">
-					<Tabs defaultValue="Login" className="w-[400px]">
-						<TabsList className="grid w-full grid-cols-2">
-							<TabsTrigger value="Login">LogIn</TabsTrigger>
-							<TabsTrigger value="SignUp">SignUp</TabsTrigger>
-						</TabsList>
-						<TabsContent value="Login">
-							<Card>
-								<CardHeader>
-									<CardTitle>Login to your Account</CardTitle>
-								</CardHeader>
-								<CardContent className="space-y-2">
-									<LoginForm />
-								</CardContent>
-							</Card>
-						</TabsContent>
-						<TabsContent value="SignUp">
-							<Card>
-								<CardHeader>
-									<CardTitle>Create a new Account</CardTitle>
-								</CardHeader>
-								<CardContent className="space-y-2">
-									<SignUpForm />
-								</CardContent>
-							</Card>
-						</TabsContent>
-					</Tabs>
-				</DialogContent>
-			</Dialog>
-		</>
-	);
+const LoginPopup: React.FC<LoginPopupProps> = ({
+  btntext,
+  btnVaraint,
+  icon = true,
+  classname = "",
+}) => {
+  return (
+    <>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant={btnVaraint} className={classname}>
+            {icon && <Login size="32" className="pr-2" />}
+            {btntext}
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="justify-center w-[350px] md:w-[500px] pt-10 md:pt-10 ">
+          <Tabs defaultValue="Login" className="w-[300px] md:w-[400px]">
+            <TabsList className="grid w-full grid-cols-2">
+              <TabsTrigger value="Login">LogIn</TabsTrigger>
+              <TabsTrigger value="SignUp">SignUp</TabsTrigger>
+            </TabsList>
+            <TabsContent value="Login">
+              <Card>
+                <CardHeader>
+                  <CardTitle>Login to your Account</CardTitle>
+                </CardHeader>
+                <CardContent className="space-y-2">
+                  <LoginForm />
+                </CardContent>
+              </Card>
+            </TabsContent>
+            <TabsContent value="SignUp">
+              <Card>
+                <CardHeader>
+                  <CardTitle>Create a new Account</CardTitle>
+                </CardHeader>
+                <CardContent className="space-y-2">
+                  <SignUpForm />
+                </CardContent>
+              </Card>
+            </TabsContent>
+          </Tabs>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
 };
 
 export default LoginPopup;
